8360e6357522ca7d39b816cb3dab3d9f410b7659
Uduak Akpabio <udlive@yahoo.com>
2024-06-21 11:17:33 +0100

    fix font

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,5 @@
-import { Montserrat } from 'next/font/google'
 import './globals.css'
 import MainComponent from "@/components/MainComponent"
-
-const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'myAssistant',
@@ -12,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className}`}>
+      <body>
         <MainComponent children={children}/>
       </body>
     </html>
